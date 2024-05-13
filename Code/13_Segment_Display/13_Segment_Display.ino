386a21c369e9c1bb1da6b681461dd1f7d5ae7dcb
IbrahimOladepo <37977406+IbrahimOladepo@users.noreply.github.com>
2024-05-13 18:42:07 -0500

    Setting up main 13 segment display code

--- Code/13_Segment_Display/13_Segment_Display.ino
+++ Code/13_Segment_Display/13_Segment_Display.ino
@@ -1,12 +1,66 @@
-#include "13_Segment_Display.h"
+#include <FastLED.h>
+
+
+// Define Arduino pin connections
+#define LED_PIN_1     7
+#define LED_PIN_2     8
+
+// Define LED and digit parameters
+#define NUM_LEDS    13
+#define DIGIT       10
+
+
+// Define LED display pattern for each digit
+const char segmentPatterns[DIGIT][NUM_LEDS] = {
+
+  // 0, 1, 2, 3, 4, 5, 6, 7, 8, 9,10,11,12
+    {1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1}, // 0
+    {1, 1, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0}, // 1
+    {1, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 1}, // 2
+    {1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0}, // 3
+    {1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0}, // 4
+    {0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0}, // 5
+    {0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1}, // 6
+    {1, 1, 1, 3, 0, 0, 0, 1, 1, 1, 0, 0, 0}, // 7
+    {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1}, // 8
+    {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0}  // 9
+
+};
+
+
+// Time to display 
+int timeNow = 0;
+
+
+// Init LEDs
+CRGB leds_1[NUM_LEDS];
+CRGB leds_2[NUM_LEDS];
+
+
+// Function definitions
+unsigned int count(unsigned int i);
 
 
 void setup() {
-  // put your setup code here, to run once:
+  
+  // Initialize FastLED
+  FastLED.addLeds<WS2812, LED_PIN_1, GRB>(leds_1, NUM_LEDS);
+  FastLED.addLeds<WS2812, LED_PIN_2, GRB>(leds_2, NUM_LEDS);
 
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
 
+void loop() {
+  
+  // Time increment
+  timeNow = (timeNow + 1) % 24;
+
+}
+
+
+unsigned int count(unsigned int i) {
+  
+  unsigned int ret=1;
+  while (i/=10) ret++;
+  return ret;
 }
