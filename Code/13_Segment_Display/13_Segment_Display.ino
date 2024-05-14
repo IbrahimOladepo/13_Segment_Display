0fcf4e090cda562bcee58f9103ba2a2b1b4d507e
IbrahimOladepo <37977406+IbrahimOladepo@users.noreply.github.com>
2024-05-14 00:49:28 -0500

    Added code to display digit sequence on dual display

--- Code/13_Segment_Display/13_Segment_Display.ino
+++ Code/13_Segment_Display/13_Segment_Display.ino
@@ -21,24 +21,46 @@ const char segmentPatterns[DIGIT][NUM_LEDS] = {
     {1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0}, // 4
     {0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0}, // 5
     {0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1}, // 6
-    {1, 1, 1, 3, 0, 0, 0, 1, 1, 1, 0, 0, 0}, // 7
+    {1, 1, 1, 1, 0, 0, 0, 1, 1, 1, 0, 0, 0}, // 7
     {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1}, // 8
     {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0}  // 9
 
 };
 
 
+// Color pattern options
+int colorPattern[7][3] = {
+  {255, 0, 0},
+  {0, 255, 0},
+  {0, 0, 255},
+  {150, 0, 255},
+  {255, 200, 20},
+  {85, 60, 180},
+  {50, 255, 20}
+};
+
+
+// Display reset pattern
+int offPattern[] = {0, 0, 0};
+
+
 // Time to display 
 int timeNow = 0;
 
+// Color selector variable
+int colorNow = 5;
+
+// Color time display variable
+int colorTime = 100;
+
 
 // Init LEDs
-CRGB leds_1[NUM_LEDS];
-CRGB leds_2[NUM_LEDS];
+CRGB leds_1[NUM_LEDS];    // RIGHT LED (UNIT)
+CRGB leds_2[NUM_LEDS];    // LEFT LED (TENS)
 
 
 // Function definitions
-unsigned int count(unsigned int i);
+unsigned int digitCount(unsigned int i);     // Counts the number of digits in a given number
 
 
 void setup() {
@@ -51,14 +73,100 @@ void setup() {
 
 
 void loop() {
+
+  // Count digits
+  unsigned int digits = digitsCount(timeNow);
+
+  // Loop through each digit
+  // and turn on LEDs accordingly
+  if (digits = 1){
+
+    // Reset display
+    for (int i = (NUM_LEDS - 1); i >= 0; i--){
+
+      leds_1[i] = CRGB(offPattern[0], offPattern[1], offPattern[2]);
+      FastLED.show();
+
+    }
+    
+    // Turn on single display
+    for (int i = 0; i < NUM_LEDS; i++){
+
+      if (segmentPatterns[timeNow][i] == 1){
+
+        leds_1[i] = CRGB(colorPattern[colorNow][0], colorPattern[colorNow][1], colorPattern[colorNow][2]);
+        FastLED.show();
+        delay(colorTime); 
+
+      }
+
+    }
+
+  }
+  else{
+
+    // Splits digits through a loop
+    char digitsArray[digits];
+    while (digits--) {
+      digitsArray[digits] = (timeNow % 10);
+      timeNow /= 10;
+    }
+
+    // Turn on both displays
+    // DISPLAY 1
+    // Reset display
+    for (int i = (NUM_LEDS - 1); i >= 0; i--){
+
+      leds_1[i] = CRGB(offPattern[0], offPattern[1], offPattern[2]);
+      FastLED.show();
+
+    }
+
+    for (int i = 0; i < NUM_LEDS; i++){
+
+      if (segmentPatterns[digitsArray[0]][i] == 1){
+
+        leds_1[i] = CRGB(colorPattern[colorNow][0], colorPattern[colorNow][1], colorPattern[colorNow][2]);
+        FastLED.show();
+        delay(colorTime); 
+
+      }
+
+    }
+
+    // DISPLAY 2
+    // Reset display
+    for (int i = (NUM_LEDS - 1); i >= 0; i--){
+
+      leds_2[i] = CRGB(offPattern[0], offPattern[1], offPattern[2]);
+      FastLED.show();
+
+    }
+
+    for (int i = 0; i < NUM_LEDS; i++){
+
+      if (segmentPatterns[digitsArray[1]][i] == 1){
+
+        leds_2[i] = CRGB(colorPattern[colorNow][0], colorPattern[colorNow][1], colorPattern[colorNow][2]);
+        FastLED.show();
+        delay(colorTime); 
+
+      }
+
+    }
+
+  }
   
   // Time increment
   timeNow = (timeNow + 1) % 24;
+  delay(colorTime*5); 
 
 }
 
 
-unsigned int count(unsigned int i) {
+// Code from: 
+// https://stackoverflow.com/questions/9302681/c-how-to-break-apart-a-multi-digit-number-into-separate-variables
+unsigned int digitsCount(unsigned int i) {
   
   unsigned int ret=1;
   while (i/=10) ret++;
